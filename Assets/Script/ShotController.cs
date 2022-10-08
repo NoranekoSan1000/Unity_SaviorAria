ee7057a37dfc1b6e87f31060ce3a733d3e1a0105
NoranekoSan1000 <88626463+NoranekoSan1000@users.noreply.github.com>
2022-10-08 14:10:42 +0900

    feature shot

--- Assets/Script/ShotController.cs
+++ Assets/Script/ShotController.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class ShotController : MonoBehaviour
 {
+    public GameObject Origin_Shot;
+    public GameObject Bullet;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,15 @@ public class ShotController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKey(KeyCode.T))
+        {
+            Debug.Log("Shot Success");
+            GameObject Copy_Shot = Instantiate(Origin_Shot) as GameObject;
+            Copy_Shot.tag = "Shot";
+            Copy_Shot.transform.position = Bullet.transform.position;
+            Vector3 force;
+            force = Bullet.transform.forward * 1;
+            Copy_Shot.GetComponent<Rigidbody>().AddForce(force);
+        }
     }
 }
