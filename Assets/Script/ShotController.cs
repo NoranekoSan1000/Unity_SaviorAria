b49d920c560c83dbc779c57c69d38b1e9f9e7cec
NoranekoSan1000 <88626463+NoranekoSan1000@users.noreply.github.com>
2022-10-08 15:23:46 +0900

    feature/shotSE

--- Assets/Script/ShotController.cs
+++ Assets/Script/ShotController.cs
@@ -7,10 +7,13 @@ public class ShotController : MonoBehaviour
     public GameObject Origin_Shot;
     public GameObject Bullet;
 
+    public AudioClip SE_Shot;
+    AudioSource audioSource;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -18,19 +21,19 @@ public class ShotController : MonoBehaviour
     {
         if (OVRInput.GetDown(OVRInput.RawButton.RIndexTrigger))
         {
-            Debug.Log("Shot Success");
+            audioSource.PlayOneShot(SE_Shot);
             GameObject Copy_Shot = Instantiate(Origin_Shot) as GameObject;
             Copy_Shot.tag = "Shot";
             Copy_Shot.transform.position = Bullet.transform.position;
             Vector3 force;
-            force = Bullet.transform.forward * 800;
+            force = Bullet.transform.forward * 1000;
             Copy_Shot.GetComponent<Rigidbody>().AddForce(force);
         }
 
         //keyboard—p
         if(Input.GetKey(KeyCode.T))
         {
-            Debug.Log("Shot Success");
+            audioSource.PlayOneShot(SE_Shot);
             GameObject Copy_Shot = Instantiate(Origin_Shot) as GameObject;
             Copy_Shot.tag = "Shot";
             Copy_Shot.transform.position = Bullet.transform.position;
